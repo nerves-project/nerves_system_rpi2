a26c83a067aab74e732b9f45f44314ef8775d204
Frank Hunleth <fhunleth@troodon-software.com>
2017-12-08 14:37:11 -0500

    Sync mix.exs w/ rpi0 version
    This makes it easier to compare.

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule NervesSystemRpi2.Mixfile do
       app: @app,
       version: @version,
       elixir: "~> 1.4",
-      compilers: Mix.compilers ++ [:nerves_package],
+      compilers: Mix.compilers() ++ [:nerves_package],
       nerves_package: nerves_package(),
       description: description(),
       package: package(),
@@ -27,8 +27,6 @@ defmodule NervesSystemRpi2.Mixfile do
   def nerves_package do
     [
       type:  :system,
-      version: @version,
-      compiler: :nerves_package,
       artifact_url: [
         "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz"
       ],
@@ -43,8 +41,8 @@ defmodule NervesSystemRpi2.Mixfile do
   defp deps do
     [
       {:nerves, "~> 0.8", runtime: false},
-      {:nerves_system_br, "~> 0.15.0", runtime: false, app: false},
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.12.1", runtime: false, app: false}
+      {:nerves_system_br, "~> 0.15.1", runtime: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.12.1", runtime: false},
     ]
   end
 
