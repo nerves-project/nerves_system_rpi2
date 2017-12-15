dd0c75242b54a7abc505ea1330683fb14f909fab
Frank Hunleth <fhunleth@troodon-software.com>
2017-12-15 14:18:47 -0500

    Bump nerves_system_br; sync up revert changes

--- post-build.sh
+++ post-build.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-if [ -z $TARGET_DIR ]; then
-    echo "Uh oh, TARGET_DIR isn't set. That's weird."
-    exit 1
-fi
-
+# Create the revert script for manually switching back to the previously
+# active firmware.
 mkdir -p $TARGET_DIR/usr/share/fwup
 $HOST_DIR/usr/bin/fwup -c -f $NERVES_DEFCONFIG_DIR/fwup-revert.conf -o $TARGET_DIR/usr/share/fwup/revert.fw
